00b26f7e61b5dc46c15968d44570a4b6cb272808
VlaKarlo <116498296+VlaKarlo@users.noreply.github.com>
2025-01-09 16:26:37 +0100

    lessHardcoding

--- script/script.js
+++ script/script.js
@@ -3,55 +3,58 @@ window.onload = function(){
 
     if(window.matchMedia("(min-width:2560px)").matches){
 
-        document.getElementById("frontendShape").coords = "555,1063,956,1225";
-        document.getElementById("graphicShape").coords = "2091,85,2371,455";
+        document.getElementById("frontendShape").coords = "555,1063,952,1221";
+        document.getElementById("graphicShape").coords = "2072,80,2351,453";
         document.getElementById("pythonShape").coords = "100,969,364,1107";
         document.getElementById("softSkillsShape").coords = "290,75,495,281";
-        document.getElementById("CShape").coords = "1200,1047,1310,1247";
-        document.getElementById("C++Shape").coords = "1665,1221,1855,1331";
-        document.getElementById("C#Shape").coords = "1656,993,1855,1100";
-        document.getElementById("arduinoShape").coords = "2157,1074,2425,1211";
+        document.getElementById("CShape").coords = "1193,1047,1303,1247";
+        document.getElementById("C++Shape").coords = "1642,1221,1841,1331";
+        document.getElementById("C#Shape").coords = "1642,993,1841,1100";
+        document.getElementById("arduinoShape").coords = "2138,1074,2407,1211";
 
     }
 
     if(window.matchMedia("(min-width:1920px)").matches && window.matchMedia("(max-width:2559px)").matches){
         
         document.getElementById("frontendShape").coords = "412,795,720,918";
-        document.getElementById("graphicShape").coords = "1562,55,1783,344";
-        document.getElementById("pythonShape").coords = "72,724,276,830";
-        document.getElementById("softSkillsShape").coords = "217,52,371,211";
-        document.getElementById("CShape").coords = "898,781,985,936";
-        document.getElementById("C++Shape").coords = "1240,913,1395,1000";
-        document.getElementById("C#Shape").coords = "1240,740,1395,830";
-        document.getElementById("arduinoShape").coords = "1615,800,1822,912";
+        document.getElementById("graphicShape").coords = "1549,60,1760,344";
+        document.getElementById("pythonShape").coords = "72,729,272,830";
+        document.getElementById("softSkillsShape").coords = "217,55,371,211";
+        document.getElementById("CShape").coords = "892,786,974,936";
+        document.getElementById("C++Shape").coords = "1229,913,1379,1000";
+        document.getElementById("C#Shape").coords = "1229,745,1379,830";
+        document.getElementById("arduinoShape").coords = "1603,806,1800,909";
 
     }
 
     if(window.matchMedia("(min-width:1280px)").matches && window.matchMedia("(max-width:1919px)").matches){
 
         document.getElementById("frontendShape").coords = "275,530,476,611";
-        document.getElementById("graphicShape").coords = "1044,37,1185,230";
+        document.getElementById("graphicShape").coords = "1027,40,1167,230";
         document.getElementById("pythonShape").coords = "50,484,182,555";
         document.getElementById("softSkillsShape").coords = "143,36,250,140";
-        document.getElementById("CShape").coords = "600,522,660,626";
-        document.getElementById("C++Shape").coords = "826,611,928,665";
-        document.getElementById("C#Shape").coords = "828,500,928,550";
-        document.getElementById("arduinoShape").coords = "1080,537,1211,606";
+        document.getElementById("CShape").coords = "592,522,646,626";
+        document.getElementById("C++Shape").coords = "814,611,914,665";
+        document.getElementById("C#Shape").coords = "814,495,914,550";
+        document.getElementById("arduinoShape").coords = "1062,537,1193,606";
 
     }
 
     if(window.matchMedia("(min-width:800px)").matches && window.matchMedia("(max-width:1279px)").matches){
 
-        document.getElementById("frontendShape").coords = "174,443,300,509";
-        document.getElementById("graphicShape").coords = "653,33,742,191";
-        document.getElementById("pythonShape").coords = "31,404,116,460";
-        document.getElementById("softSkillsShape").coords = "90,31,158,117";
-        document.getElementById("CShape").coords = "375,437,410,520";
-        document.getElementById("C++Shape").coords = "518,508,580,554";
-        document.getElementById("C#Shape").coords = "518,413,581,460";
-        document.getElementById("arduinoShape").coords = "673,447,758,505";
+        document.getElementById("frontendShape").coords = "169,323,292,372";
+        document.getElementById("graphicShape").coords = "636,25,723,140";
+        document.getElementById("pythonShape").coords = "31,294,112,336";
+        document.getElementById("softSkillsShape").coords = "90,23,152,86";
+        document.getElementById("CShape").coords = "366,318,401,379";
+        document.getElementById("C++Shape").coords = "504,371,566,405";
+        document.getElementById("C#Shape").coords = "504,301,566,336";
+        document.getElementById("arduinoShape").coords = "657,327,740,368";
 
     }
+
+    toggleLanguagesDisplay();
+    resetFrontendPriority();
 
 };
 
@@ -68,6 +71,11 @@ function resetFrontendPriority(){
     document.getElementById("frontendJSTab").style.backgroundColor = "#1e1f1d";
     document.getElementById("JSButton").style.backgroundColor = "#1e1f1d";
     document.getElementById("frontendURLText").innerHTML = "https://github.com/VlaKarlo";
+
+    writeHTMLCode();
+    writeCSSCode();
+    writeJSCode();
+
 }
 
 function makeHTMLPriority(){
@@ -83,6 +91,11 @@ function makeHTMLPriority(){
     document.getElementById("frontendJSTab").style.backgroundColor = "#1e1f1d";
     document.getElementById("JSButton").style.backgroundColor = "#1e1f1d";
     document.getElementById("frontendURLText").innerHTML = "https://github.com/VlaKarlo/HTML";
+
+    writeHTMLCode();
+    clearCSSCode();
+    clearJSCode();
+
 }
 
 function makeCSSPriority(){
@@ -98,6 +111,11 @@ function makeCSSPriority(){
     document.getElementById("frontendJSTab").style.backgroundColor = "#1e1f1d";
     document.getElementById("JSButton").style.backgroundColor = "#1e1f1d";
     document.getElementById("frontendURLText").innerHTML = "https://github.com/VlaKarlo/CSS";
+
+    clearHTMLCode();
+    writeCSSCode();
+    clearJSCode();
+
 }
 
 function makeJSPriority(){
@@ -113,6 +131,123 @@ function makeJSPriority(){
     document.getElementById("frontendJSTab").style.backgroundColor = "#58416d";
     document.getElementById("JSButton").style.backgroundColor = "#58416d";
     document.getElementById("frontendURLText").innerHTML = "https://github.com/VlaKarlo/JS";
+
+    clearHTMLCode();
+    clearCSSCode();
+    writeJSCode();
+
+}
+
+function writeHTMLCode(){
+    document.getElementById("HTMLBody").innerHTML = `
+    <br>
+                            &#60;!DOCTYPE html&#62; <br>
+                            &#60;html lang="en"&#62; <br>
+                            &#60;head&#62; <br>
+                                &emsp; &#60;meta charset="UTF-8"&#62; <br>
+                                &emsp; &#60;meta name="viewport" content="width=device-width, initial-scale=1.0"&#62;<br>
+                                &emsp; &#60;title&#62; Karlo Vlašić&#60;/title&#62; <br>
+                                &emsp; &#60;link rel="icon" type="image/x-icon" href="/images/favicon.ico"&#62; <br>
+                                &emsp; &#60;link rel="stylesheet" href="style/style.css"&#62; <br>
+                                &emsp; &#60;script src="script/script.js"&#62;&#60;/script&#62; <br>
+                            &#60;/head&#62; <br>
+                            &#60;body&#62; <br>
+                                &emsp;&#60;main&#62; <br>
+                                    &emsp;&emsp;&#60;section id="PCB"&#62; <br>
+                                        &emsp;&emsp;&emsp;&#60;img id="PCBImage" src="images/map/map.png" usemap="#PCB" width="1920" height="1080"&#62; <br>
+                                        &emsp;&emsp;&emsp;&#60;map name="PCB"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#60;area shape="rect" coords="412,795,720,918" alt="frontend" href="#frontend"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#60;area shape="rect" coords="1562,55,1783,344" alt="graphicDesign" href="#graphicDesign"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#60;area shape="rect" coords="72,724,276,830" alt="python" href="#python"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#60;area shape="rect" coords="217,52,371,211" alt="softSkills" href="#softSkills"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#60;area shape="rect" coords="898,781,985,936" alt="C" href="#programming"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#60;area shape="rect" coords="1240,913,1395,1000" alt="C++" href="#programming"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#60;area shape="rect" coords="1240,740,1395,830" alt="C#" href="#programming"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#60;area shape="rect" coords="1615,800,1822,912" alt="arduino" href="#programming"&#62; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#9679; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#9679; <br>
+                                            &emsp;&emsp;&emsp;&emsp;&#9679; <br>
+                                &emsp;&#60;/footer&#62; <br>
+                            &#60;/body&#62; <br>
+                            &#60;/html&#62; <br>
+    `
+    ;
+}
+
+function clearHTMLCode(){
+    document.getElementById("HTMLBody").innerHTML = "";
+}
+
+function writeCSSCode(){
+    document.getElementById("CSSBody").innerHTML = `
+    <br>
+    #frontend{ <br>
+        &emsp;text-align: center;<br>
+    }<br>
+    <br>
+    #page{<br>
+        &emsp;border-radius: 30px;<br>
+        &emsp;padding: 10px 0;<br>
+        &emsp;margin: 0px 10px;<br>
+        &emsp;background-color: #2c2e2c;<br>
+    }<br>
+    <br>
+    #frontendHeader{<br>
+        &emsp;display: flex;<br>
+    }<br>
+    <br>
+    #frontendHeaderTab img, #frontendHTMLTab img, #frontendCSSTab img, #frontendJSTab img{<br>
+        &emsp;margin-top: 10px;<br>
+        &emsp;margin-left: 10px;<br>
+        &emsp;margin-bottom: 5px;<br>
+    }<br>
+    <br>
+    #frontendHeaderTab, #frontendHTMLTab, #frontendCSSTab, #frontendJSTab{<br>
+        &emsp;display: flex;<br>
+        &emsp;text-align: left;<br>
+        &emsp;background-color: #1e1f1d;<br>
+        &emsp;border-radius: 30px 30px 0 0;<br>
+    }<br>
+    `
+    ;
+}
+
+function clearCSSCode(){
+    document.getElementById("CSSBody").innerHTML = "";
+}
+
+function writeJSCode(){
+    document.getElementById("JSBody").innerHTML = `
+        <br>
+        function resetFrontendPriority(){<br>
+            &emsp;document.getElementById("HTML").style.display = "block";<br>
+            &emsp;document.getElementById("CSS").style.display = "block";<br>
+            &emsp;document.getElementById("JS").style.display = "block";<br>
+        }<br>
+        <br>
+        function makeHTMLPriority(){<br>
+            &emsp;document.getElementById("HTML").style.display = "block";<br>
+            &emsp;document.getElementById("CSS").style.display = "none";<br>
+            &emsp;document.getElementById("JS").style.display = "none";<br>
+        }<br>
+        <br>
+        function makeCSSPriority(){<br>
+            &emsp;document.getElementById("HTML").style.display = "none";<br>
+            &emsp;document.getElementById("CSS").style.display = "block";<br>
+            &emsp;document.getElementById("JS").style.display = "none";<br>
+        }<br>
+        <br>
+        function makeJSPriority(){<br>
+            &emsp;document.getElementById("HTML").style.display = "none";<br>
+            &emsp;document.getElementById("CSS").style.display = "none";<br>
+            &emsp;document.getElementById("JS").style.display = "block";<br>
+        }<br>
+    `
+    ;
+}
+
+function clearJSCode(){
+    document.getElementById("JSBody").innerHTML = "";
 }
 
 function toggleLanguagesDisplay() {
